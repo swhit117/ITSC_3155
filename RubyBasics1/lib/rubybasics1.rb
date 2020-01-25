f745e8e742d7ed861e45022422c9142e6ac77f76
EC2 Default User <ec2-user@ip-172-31-89-212.ec2.internal>
2020-01-25 00:49:14 +0000

    RubyBasics1 - Part 3 V2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -13,8 +13,27 @@ def max_2_sum arr
     0
   end
 end
-
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if (arr.length == 0)
+    return false
+  end
+  if (arr.length == 1)
+    return false
+  end
+  sorted_arr = arr.sort
+  head =0
+  tail = sorted_arr.length-1
+  
+  while head < tail 
+    current_sum = sorted_arr[head] + sorted_arr[tail]
+    if current_sum == n
+      return true
+    elsif current_sum < n
+      head = head + 1
+    else 
+      tail = tail - 1
+    end
+  end
+  return false
 end

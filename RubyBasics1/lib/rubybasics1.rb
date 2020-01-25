2618c614c2cfd22da0c029f0c519b937d586aa82
EC2 Default User <ec2-user@ip-172-31-89-212.ec2.internal>
2020-01-25 00:17:36 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,14 @@
+# Lab 1
+# Part I
+def sum arr
+  arr.inject(0) {|sum, i|  sum + i }
+end
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end

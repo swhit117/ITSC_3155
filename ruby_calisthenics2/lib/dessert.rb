5f79703f9fb83d0d1928cac01b36d83452fa3f6f
EC2 Default User <ec2-user@ip-172-31-89-212.ec2.internal>
2020-02-18 12:53:17 +0000

    Lab 11 initial commit

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -1,19 +1,52 @@
 class Dessert
   # add code for setters and getters
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
+  end
+  def name=(inName)
+    @name = inName
+  end
+  def calories=(inCalories)
+    @calories = inCalories
+  end
+  def name
+    @name
+  end
+  def calories
+    @calories
   end
   def healthy?
-    # your code here
+    if @calories < 200
+      return true
+    else
+      return false
+    end
   end
   def delicious?
-    # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
   # add code for setters and getters
   def initialize(flavor)
-    # your code here
+   @calories = 5
+   @name = flavor
+   @name += " jelly bean"
+   @flavor = flavor
+  end
+  def flavor=(inFlavor)
+    @flavor = inFlavor
+  end
+  def flavor
+    @flavor
+  end
+  def delicious?
+    if (@flavor != "licorice")
+      return true
+    else
+      return false
+    end
   end
 end
